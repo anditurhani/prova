f2bd450bd932211421292ff2818e1b4ea96d266d
anditurhani <work.anditurhani@hotmail.com>
2020-08-31 09:51:54 +0200

    Modifica metodo getAllContinents

--- src/it/objectmethod/world/dao/ICountryDao.java
+++ src/it/objectmethod/world/dao/ICountryDao.java
@@ -2,13 +2,14 @@ package it.objectmethod.world.dao;
 
 import java.util.List;
 
+import it.objectmethod.world.model.Continent;
 import it.objectmethod.world.model.Country;
 
 public interface ICountryDao {
 	
 	public List<Country> getCountryListByContinent(String continent);
 
-	public List<String> getAllContinents();
+	public List<Continent> getAllContinents();
 
 	public List<Country> getAllCountries();
 	
