2250bc064be26810fb0e0dcff06c32ec36d4520c
anditurhani <work.anditurhani@hotmail.com>
2020-08-31 09:55:09 +0200

    First commit

---
+++ src/it/objectmethod/world/model/Continent.java
@@ -0,0 +1,15 @@
+package it.objectmethod.world.model;
+
+public class Continent {
+	
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
